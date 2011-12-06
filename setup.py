1531fb8c57572de7f44d7a35975ebed6fc4a0a8b
Emil Loer <emil@koffietijd.net>
2011-12-06 10:10:52 +0100

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="mnml",
-    version="0.1.0",
+    version="0.1.1",
     description="Parsing package for the Mnml language",
     author="Emil Loer",
     author_email="emil@koffietijd.net",
